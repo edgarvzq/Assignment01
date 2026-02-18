285e73c52eb894431e25efab1475ea2119313b25
Edgar Vazquez <edgar.vzq.b@gmail.com>
2026-02-18 13:32:57 -0800

    Completed Step 4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 // STEP 1
+//let someMonth
+//function theMonth()
+//let currentMonth
+//let summerMonth
+//let myLibraryFunction
+
 // STEP 2
+// 10
+// 'Edgar Vazquez'
+// let firstName = 'Edgar'
+// false
+// null
+
 // STEP 3
+// let sum = 2 + 2
+// let mpg = milesDriven / gallons;
+
+// STEP 4
+// Camel case
+let firstName, lastName
+let address, city, state, zipCode 
+let yourAge 
+let referralSource 
+let mayWeContactYou
+
+// Hingarian notation
+let strFirstName, strLastName 
+let srtAddress, strCity, strState, intZipCode 
+let intYourAge 
+let strReferralSource 
+let blnMayWeContactYou
+
+// STEP 5
